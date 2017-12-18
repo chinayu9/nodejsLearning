674f2464af3adc19645d7c4e9a152265ea92806b
yu9iang <chinayu9iang@gmail.com>
2017-12-18 09:40:25 +0800

    update

--- httpRequestTest.js
+++ httpRequestTest.js
@@ -1,47 +1,9 @@
 let http = require('http');
 let fs = require('fs');
 let url = require('url');
-//console.log(url.parse('http://movie.douban.com/subject/11529526/'));
-let opt = {
-	host:'10.248.174.61',
-	port:8124,
-	path:'http://github.com/request/request/issues/2047',
-	timeout:5000
-	/*
-	headers:{
-		'Accept':'text\/html,application\/xhtml+xml,application\/xml;q=0.9,image\/webp,image\/apng,*\/*;q=0.8',
-		'Accept-Encoding':'gzip, deflate',
-		'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6',
-		'Cache-Control':'no-cache',
-		'Cookie':'PHPSESSID=web1~ofla022s714jl4f9g2m33brcq0; afpCT=1; sf_remember=e890fee81d73ba964bbdeb2629c9956d; _ga=GA1.2.645899979.1505461181; _gid=GA1.2.195932116.1505693040; _gat=1; Hm_lvt_e23800c454aa573c0ccb16b52665ac26=1506045241,1506050439,1506058498,1506061375; Hm_lpvt_e23800c454aa573c0ccb16b52665ac26=1506066842; io=slI06GtQXIlm-yhMO_JX',
-		'Host':'segmentfault.com',
-		'Referer':'https://segmentfault.com/t/node.js',
-		
-	}
-	*/
-};
 
-let req = http.request(opt,(res)=>{
-	let html = '';
-	res.on('data',(chunk)=>{
-		console.log('success');
-		console.log(chunk.toString());
-		html += chunk;
-	});
-	res.on('end',()=>{
-		let writeStream = fs.createWriteStream('./httpRequest.html');
-		writeStream.write(html);
-		writeStream.end();
-	});
-});
-req.on('error',(err)=>{
-	console.log(err.message);
-});
-req.end();
-
-
-/*
-http.get(opt,(res)=>{
+http.get("http://www.baidu.com",(res)=>{
+	console.log("start.....");
 	let html = '';
 	res.on('data',(chunk)=>{
 		html += chunk;
@@ -52,4 +14,3 @@ http.get(opt,(res)=>{
 		writeStream.end();
 	});
 });
-*/
---
+++ nodejs.md
@@ -0,0 +1,8 @@
+npm config set proxy http://server:port  代理设置
+npm config delete proxy  取消代理
+npm install packageName -g   全局安装package
+npm update express         更新模块
+npm init 生成package.json文件
+npm install -g cnpm --registry=https://registry.npm.taobao.org 使用淘宝NPM镜像
+cnpm install packageName   使用淘宝npm镜像安装各种模块
+npm config ls -l     查看npm config
